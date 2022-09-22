f9c804ec6a85037f9673c98ca7bf82a2fe2f2759
Kris! <42949874+krstphrrr@users.noreply.github.com>
2022-09-22 14:12:24 -0600

    added fields to tblSchema

--- nest/src/tbl-schema/dto/get-tblschema.dto.ts
+++ nest/src/tbl-schema/dto/get-tblschema.dto.ts
@@ -1,32 +1,118 @@
 import { ApiProperty, ApiQuery } from '@nestjs/swagger';
+import { Prisma } from '@prisma/client';
 import { Transform, Type } from 'class-transformer';
 
 export class dtoTblSchema {
-  @Transform(({ value }) => value.trim().split(',').map(id=>Number(id)))
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => Number(id)),
+  )
   rid: number;
 
-  @Transform(({ value }) => value.trim().split(',').map(id=>String(id)))
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => Number(id)),
+  )
+  Order: Prisma.Decimal;
+
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => String(id)),
+  )
   Table: string;
 
-  @Transform(({ value }) => value.trim().split(',').map(id=>String(id)))
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => String(id)),
+  )
   Field: string;
 
-  @Transform(({ value }) => value.trim().split(',').map(id=>String(id)))
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => String(id)),
+  )
   Alias: string;
 
-  @Transform(({ value }) => value.trim().split(',').map(id=>String(id)))
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => String(id)),
+  )
   Description: string;
 
-  @Transform(({ value }) => value.trim().split(',').map(id=>String(id)))
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => String(id)),
+  )
   DataType: string;
 
-  @Transform(({ value }) => value.trim().split(',').map(id=>String(id)))
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => String(id)),
+  )
   Length: string;
 
-  @Transform(({ value }) => value.trim().split(',').map(id=>String(id)))
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => String(id)),
+  )
   DatatypeNotes: string;
 
-  @Transform(({ value }) => value.trim().split(',').map(id=>Number(id)))
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => Number(id)),
+  )
+  Min: Prisma.Decimal;
+
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => Number(id)),
+  )
+  Max: Prisma.Decimal;
+
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => String(id)),
+  )
+  Unit: string;
+
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => Number(id)),
+  )
+  SigFig: Prisma.Decimal;
+
+  @Transform(({ value }) =>
+    value
+      .trim()
+      .split(',')
+      .map((id) => Number(id)),
+  )
   Version: string;
 
   @Type(() => Date)
