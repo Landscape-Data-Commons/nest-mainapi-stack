0ae4a4dff50607a11cfe7e2821d8431a2246d541
Kris! <42949874+krstphrrr@users.noreply.github.com>
2022-10-07 11:24:07 -0600

    added like operator to soil stab

--- nest/src/datasoilstability/datasoilstability.controller.ts
+++ nest/src/datasoilstability/datasoilstability.controller.ts
@@ -14,9 +14,9 @@ export class DatasoilstabilityController {
   @ApiOkResponse({ type: DatasoilstabilityEnt, isArray: true })
   GetSoilStability(
     @CustomRequestObjHandler(dtoDataSoilStability) ValidatedParams?: dtoDataSoilStability){
-    for (const [key, value] of Object.entries(ValidatedParams)) {
+    for (const [key, value] of Object.entries(ValidatedParams['params'])) {
       if (Array.isArray(value)) {
-        ValidatedParams[key] = { in: value };
+        ValidatedParams['params'][key] = { in: value };
       }
     }
     return this.datasoilstabilityService.FindManySoilStability(ValidatedParams);
--- nest/src/datasoilstability/datasoilstability.service.ts
+++ nest/src/datasoilstability/datasoilstability.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { dataSoilStability } from '@prisma/client';
+import { LikeOperator } from 'src/CustomRequest.decorator';
 
 @Injectable()
 export class DatasoilstabilityService {
   constructor(public prisma: PrismaService) {}
 
   FindManySoilStability(params: {}): Promise<dataSoilStability[] | null>{
+    const { ...whereParams } = params['params'];
 
-    return this.prisma.dataSoilStability.findMany({
-      where: { ...params },
-    });
+    if ('wildcards' in params) {
+      const { ...wildcards } = params['wildcards'];
+      const wc = LikeOperator(wildcards);
+
+      return this.prisma.dataSoilStability.findMany({
+        where: { ...whereParams, wc },
+      });
+    } else {
+      return this.prisma.dataSoilStability.findMany({
+        where: { ...whereParams },
+      });
+    }
   }
 }
