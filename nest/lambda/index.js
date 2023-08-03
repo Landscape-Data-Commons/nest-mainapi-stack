5a190154fc24accaf8a0dc9ab36b0f17e035b892
Kris! <42949874+krstphrrr@users.noreply.github.com>
2023-08-03 12:21:48 -0600

    migrate to modular sdkv3

--- nest/lambda/index.js
+++ nest/lambda/index.js
@@ -1,4 +1,9 @@
-const { APIGateway, CognitoIdentityServiceProvider } = require('aws-sdk');
+const {
+        APIGateway
+      } = require("@aws-sdk/client-api-gateway"),
+      {
+        CognitoIdentityProvider: CognitoIdentityServiceProvider
+      } = require("@aws-sdk/client-cognito-identity-provider");
 
 const API = new APIGateway({ region: 'us-east-1' });
 const cognito = new CognitoIdentityServiceProvider({ region: 'us-east-1' });
